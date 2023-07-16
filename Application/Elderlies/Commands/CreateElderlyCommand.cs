c5bea76496834abe87fc32f5aea0bb91774f9b97
Pedro Santos <ibbupedro@gmail.com>
2023-07-16 10:30:50 -0300

    Remove adress do CreateElderlyCommand

--- Application/Elderlies/Commands/CreateElderlyCommand.cs
+++ Application/Elderlies/Commands/CreateElderlyCommand.cs
@@ -8,6 +8,5 @@ namespace Application.Elderlies.Commands
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
-        public string Address { get; set; }
     }
 }
